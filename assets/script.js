c74c93f126fadaca7a91289e4a50e015056bf3a7
Maria Mireles <mireles335@gmail.com>
2021-06-04 23:08:06 -0500

    adding html/css styles

---
+++ assets/script.js
@@ -0,0 +1,38 @@
+//Adding event lister to begin button
+// document.addEventListener("DOMContentLoaded", () => {
+//   var timeLeftDisplay = document.querySelector("#time-left");
+//   var startBtn = document.querySelector("#start-button");
+//   var timeLeft = 60;
+
+//   function countDown() {
+//     setInterval(function () {
+//       if (timeLeft <= 0) {
+//         clearInterval((timeLeft = 0));
+//       }
+//       timeLeftDisplay.innerHTML = timeLeft;
+//       timeLeft -= 1;
+//     }, 1000);
+//   }
+//   startBtn.addEventListener("click", countDown);
+// });
+
+// beginQuiz();
+
+// set att & remove the class hide from it
+
+// create a selector for the ID for hide div - querySelector/ selectelby ID
+// remove class
+
+// var questions = [
+
+//   {
+//     question: "This is the question",
+//     choices: ["choice 1", "choice 2", "choice 3"],
+//     correctAnswer: [0]
+//   },
+//   {
+//     question: "This is the question",
+//     choices: ["choice 1", "choice 2", "choice 3"],
+//     correctAnswer: [1]
+//   }
+// ]
